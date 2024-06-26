c97cdfdace788b4c1b5a0bb272caa8d043693ce9
simbahebinbo <lansheng228@163.com>
2024-06-26 15:44:30 +0800

    update

---
+++ programs/anchor-price-oracle/src/lib.rs
@@ -0,0 +1,82 @@
+use anchor_lang::prelude::*;
+
+// use solana_program;
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod anchor_price_oracle {
+    use super::*;
+
+    // pub fn update_btc_price(ctx: Context<UpdateBTCPrice>) -> Result<()> {
+    //     Ok(())
+    // }
+
+    pub fn init_btc_price(ctx: Context<InitBTCPrice>, params: InitBTCPriceParams) -> Result<()> {
+        // let mut btc_price_account = &mut ctx.accounts.btc_price_account.load_init()?;
+        // btc_price_account.val = SwitchboardDecimal {
+        //     mantissa: 0,
+        //     scale: 1,
+        // };
+        Ok(())
+    }
+}
+
+const BTC_PRICE_SEED: &[u8] = b"BTCPrice";
+
+// #[derive(Accounts)]
+// #[instruction(params: InitBTCPriceParams)] // rpc parameters hint
+// pub struct InitBTCPrice<'info> {
+//     #[account(
+//         init,
+//         seeds = [
+//         BTC_PRICE_SEED,
+//         ],
+//         bump = params.btc_price_bump,
+//         payer = payer)]
+//     pub btc_price_account: AccountLoader<'info, BTCPriceAccountData>,
+//     pub payer: AccountInfo<'info>,
+//     #[account(address = solana_program::system_program::ID)]
+//     pub system_program: AccountInfo<'info>,
+// }
+
+#[derive(Accounts)]
+// #[instruction(params: InitBTCPriceParams)] // rpc parameters hint
+pub struct InitBTCPrice<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    pub system_program: Program<'info, System>,
+    // #[account(address = solana_program::system_program::ID)]
+    // pub system_program: AccountInfo<'info>,
+}
+
+#[derive(Default, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct InitBTCPriceParams {
+    pub btc_price_bump: u8,
+}
+
+#[derive(Default, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct UpdateBTCPriceParams {}
+
+// #[derive(Accounts)]
+// #[instruction(params: UpdateBTCPriceParams)]
+// pub struct UpdateBTCPrice<'info> {
+//     pub pyth_btc_feed: AccountInfo<'info>,
+//     pub switchboard_btc_feed: AccountInfo<'info>,
+// }
+
+// #[account(zero_copy)]
+// #[derive(Default)]
+// pub struct BTCPriceAccountData {
+//     pub val: SwitchboardDecimal,
+// }
+//
+// #[zero_copy]
+// #[derive(Default, Eq, PartialEq)]
+// pub struct SwitchboardDecimal {
+//     pub mantissa: i128,
+//     pub scale: u32,
+// }
+
+
--- tests/oracle-solana-example.ts
+++ tests/oracle-solana-example.ts
@@ -1,52 +1,47 @@
-import * as anchor from '@project-serum/anchor';
-import { Program } from '@project-serum/anchor';
-import { OracleSolanaExample } from '../target/types/oracle_solana_example';
-import {
-  Keypair,
-  SystemProgram,
-} from "@solana/web3.js";
-import * as assert from "assert";
+// @ts-ignore
+import {OracleSolanaExample} from '../target/types/anchor_price_oracle';
 
-describe('oracle-solana-example', () => {
+describe('anchor-price-oracle', () => {
 
-  // Configure the client to use the local cluster.
-  const provider = anchor.Provider.env();
-  anchor.setProvider(provider);
+    // // Configure the client to use the local cluster.
+    // const provider = anchor.Provider.env();
+    // anchor.setProvider(provider);
+    //
+    // const program = anchor.workspace.OracleSolanaExample as Program<OracleSolanaExample>;
+    // const payerKeypair = Keypair.fromSecretKey(
+    //     (provider.wallet as any).payer.secretKey
+    // );
+    //
+    // before(async () => {
+    //     const [btcPriceAddress, btcPriceBump] = anchor.utils.publicKey.findProgramAddressSync(
+    //         [Buffer.from("BTCPrice")],
+    //         program.programId
+    //     );
+    //
+    //     await assert.doesNotReject(async function () {
+    //         await program.rpc.initBtcPrice({
+    //             btcPriceBump: btcPriceBump
+    //         }, {
+    //             accounts: {
+    //                 btcPriceAccount: btcPriceAddress,
+    //                 payer: payerKeypair.publicKey,
+    //                 systemProgram: SystemProgram.programId,
+    //             },
+    //         });
+    //     });
+    //
+    // });
+    //
+    // it("Works", async () => {
+    //     assert.equal(1, 1);
+    // });
 
-  const program = anchor.workspace.OracleSolanaExample as Program<OracleSolanaExample>;
-  const payerKeypair = Keypair.fromSecretKey(
-    (provider.wallet as any).payer.secretKey
-  );
+    /*it('Is initialized!', async () => {
+      // Add your test here.
+      const tx = await program.rpc.initialize({});
+      console.log("Your transaction signature", tx);
+    });*/
 
-  before(async () => {
-    const [btcPriceAddress, btcPriceBump] = anchor.utils.publicKey.findProgramAddressSync(
-      [Buffer.from("BTCPrice")],
-      program.programId
-    );
 
-    await assert.doesNotReject(async function() {
-      await program.rpc.initBtcPrice({
-        btcPriceBump: btcPriceBump
-      }, {
-        accounts: {
-          btcPriceAccount: btcPriceAddress,
-          payer: payerKeypair.publicKey,
-          systemProgram: SystemProgram.programId,
-        },
-      });
-    });
-
-  });
-
-  it("Works", async () => {
-    assert.equal(1,1);
-  });
-
-  /*it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
-  });*/
-
-  
 });
+
