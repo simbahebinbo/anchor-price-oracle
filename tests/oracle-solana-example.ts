478513970362aebd352474aa5d99ac15fbcd3902
simbahebinbo <lansheng228@163.com>
2024-06-26 16:31:09 +0800

    update

--- tests/oracle-solana-example.ts
+++ tests/oracle-solana-example.ts
@@ -1,47 +1,13 @@
+import * as anchor from "@coral-xyz/anchor";
+import {Program} from "@coral-xyz/anchor";
 // @ts-ignore
-import {OracleSolanaExample} from '../target/types/anchor_price_oracle';
+import {AnchorPriceOracle} from '../target/types/anchor_price_oracle';
 
 describe('anchor-price-oracle', () => {
 
-    // // Configure the client to use the local cluster.
-    // const provider = anchor.Provider.env();
-    // anchor.setProvider(provider);
-    //
-    // const program = anchor.workspace.OracleSolanaExample as Program<OracleSolanaExample>;
-    // const payerKeypair = Keypair.fromSecretKey(
-    //     (provider.wallet as any).payer.secretKey
-    // );
-    //
-    // before(async () => {
-    //     const [btcPriceAddress, btcPriceBump] = anchor.utils.publicKey.findProgramAddressSync(
-    //         [Buffer.from("BTCPrice")],
-    //         program.programId
-    //     );
-    //
-    //     await assert.doesNotReject(async function () {
-    //         await program.rpc.initBtcPrice({
-    //             btcPriceBump: btcPriceBump
-    //         }, {
-    //             accounts: {
-    //                 btcPriceAccount: btcPriceAddress,
-    //                 payer: payerKeypair.publicKey,
-    //                 systemProgram: SystemProgram.programId,
-    //             },
-    //         });
-    //     });
-    //
-    // });
-    //
-    // it("Works", async () => {
-    //     assert.equal(1, 1);
-    // });
+    // Configure the client to use the local cluster.
+    anchor.setProvider(anchor.AnchorProvider.env());
 
-    /*it('Is initialized!', async () => {
-      // Add your test here.
-      const tx = await program.rpc.initialize({});
-      console.log("Your transaction signature", tx);
-    });*/
-
+    const program = anchor.workspace.AnchorPriceOracle as Program<AnchorPriceOracle>;
 
 });
-
